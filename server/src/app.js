8a6dcc41150eeca6a4ffc8d6ca86b5c23b38a66c
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-07-12 12:12:29 +0530

    Add express server setup with cookie-parser and nodemon; establish MongoDB connection

---
+++ server/src/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const allowedOrigins = [
+  process.env.ORIGIN_1,
+  process.env.ORIGIN_2,
+  process.env.ORIGIN_3,
+];
+
+const app = express();
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "32kb" })); // For JSON format
+app.use(express.text({ type: "text/*", limit: "32kb" })); // For plain text format
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//print function to ensure every step is executed
+app.use((req, res, next) => {
+  console.log(`Received:`, req.method);
+  console.log(`at:`, req.url);
+  console.log(`with body:`, req.body);
+  next();
+});
+
+// routers
+// import vendorRouter from "./routes/vendor.routes.js";
+
+//vendor routes
+// app.use("/api/v1/vendor", vendorRouter);
+
+export { app };
